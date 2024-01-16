6236b6402b1f457d15a27276626b1ecdb1179fa8
chirag10101 <83183227+chirag10101@users.noreply.github.com>
2024-01-16 10:23:05 +0530

    Calling Application

---
+++ ExtendedNumericFunctions.cs
@@ -0,0 +1,103 @@
+﻿using CommonFunctions;
+using System.Text;
+
+namespace DllDemo
+{
+    public static class ExtendedNumericFunctions
+    {
+        public static string DisplayAllEvenInRange(this NumericFunctions numericFunctions, int start, int end)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (start % 2 == 1)
+                start++;
+
+            if (end % 2 == 1)
+                end--;
+
+            for (int i = start; i <= end; i += 2)
+                sb.Append(i + " ");
+
+            return sb.ToString();
+        }
+
+        public static string DisplayAllOddInRange(this NumericFunctions numericFunctions, int start, int end)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (start % 2 == 0)
+                start++;
+
+            if (end % 2 == 0)
+                end--;
+
+            for (int i = start; i <= end; i += 2)
+                sb.Append(i + " ");
+
+            return sb.ToString();
+        }
+
+        public static string DisplayAllPrimeInRange(this NumericFunctions numericFunctions, int start, int end)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = start; i <= end; i++)
+                if (NumericFunctions.IsPrime(i))
+                {
+                    sb.Append(i + " ");
+                }
+
+            return sb.ToString();
+        }
+
+        public static string DisplayTable(this NumericFunctions numericFunctions, int num)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= 10; i++)
+            {
+                sb.AppendLine($"{num} * {i} = {num*i}");
+            }
+            return sb.ToString();
+        }
+
+        public static string DisplayTables1to10(this NumericFunctions numericFunctions)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    sb.Append((i * j) + " ");
+                }
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+
+        public static string DisplayTableInRange(this NumericFunctions numericFunctions, int num1, int num2)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = num1; i <= num2; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    sb.Append((i * j) + " ");
+                }
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+        //4503
+        public static int ReverseInt(this NumericFunctions numericFunctions, int num)
+        {
+            int temp = 0;
+            int rem = 1;
+            while (num != 0)
+            {
+                rem = num % 10;
+                temp = temp * 10 + rem;
+                num /= 10;
+            }
+            return temp;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using CommonFunctions;
+
+namespace DllDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumericFunctions numericFunctions = new NumericFunctions();
+            Console.WriteLine(NumericFunctions.AddNumbers(5, 10, 15));
+            Console.WriteLine(NumericFunctions.DivideNumbers(40, 10));
+            Console.WriteLine(NumericFunctions.MultiplyNumbers(3, 5));
+            Console.WriteLine(NumericFunctions.SubtractNumbers(89, 34));
+            Console.WriteLine(NumericFunctions.IsPrime(31));
+            Console.WriteLine(NumericFunctions.IsEven(20));
+            Console.WriteLine(NumericFunctions.IsOdd(35));
+            Console.WriteLine(NumericFunctions.MaxInt(23, 35, 10, 400, 22));
+            Console.WriteLine(NumericFunctions.MinInt(23, 35, 10, 400, 22));
+            Console.WriteLine(StringFunctions.IsPalindrome("acbaa"));
+            Console.WriteLine(StringFunctions.ReverseSentence(" hello I am chirag"));
+            Console.WriteLine(StringFunctions.NoOfVowels("Hello I am chirag 123"));
+            Console.WriteLine(StringFunctions.RemoveSpaces("      Hello   I  am chirag    "));
+            Console.WriteLine(StringFunctions.NoOfIntegers("Hello I am chirag 123"));
+            Console.WriteLine(StringFunctions.NoOfConsonants("Hello I am chirag 12 3 #$%"));
+            Console.WriteLine(StringFunctions.NoOfSpecials("Hello I am chirag 12 3 #$%"));
+            Console.WriteLine(StringFunctions.SearchSubstring("pqr", "dsacdbsa"));
+            Console.WriteLine(StringFunctions.CountOfSubString("pqr", "dspqracdpqrbspqra"));
+            Console.WriteLine(numericFunctions.DisplayAllEvenInRange(31, 46));
+            Console.WriteLine(numericFunctions.DisplayAllOddInRange(30, 47));
+            Console.WriteLine(numericFunctions.DisplayAllPrimeInRange(30, 50));
+            Console.WriteLine(numericFunctions.DisplayTable(15));
+            Console.WriteLine(numericFunctions.DisplayTables1to10());
+            Console.WriteLine(numericFunctions.DisplayTableInRange(32, 39));
+            Console.WriteLine(numericFunctions.ReverseInt(4503));
+        }
+    }
+}
